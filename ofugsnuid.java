ac0ae63106f65c4153a29cdbd3c01e873f8760ae
Sean <seanmanik@gmail.com>
2023-03-13 14:19:15 +0800

    ofugsnuid.java

--- ofugsnuid.java
+++ ofugsnuid.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class ofugsnuid {
+import java.util.*;
+
+public class ofugsnuid {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        for (int i = 0; i < n; i++) {
+            arr.add(scanner.nextInt());
+        }
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            System.out.println(arr.get(i));
+        }
+    }
 }

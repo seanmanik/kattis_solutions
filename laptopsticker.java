bfd28c6d10c6a32d19c4edb8c8209fa6aa5a896c
Sean <seanmanik@gmail.com>
2023-03-13 12:19:18 +0800

    laptopsticker solution

--- laptopsticker.java
+++ laptopsticker.java
@@ -2,6 +2,17 @@ import java.util.*;
 
 public class laptopsticker {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int wc = scanner.nextInt();
+        int hc = scanner.nextInt();
+        int ws = scanner.nextInt();
+        int hs = scanner.nextInt();
+        if (wc - ws > 1) {
+            if (hc - hs > 1) {
+                System.out.println(1);
+                return;
+            }
+        }
+        System.out.println(0);
     }
 }

edcf60678a3c76af8dd04987910baf7565b9b6c0
Sean <seanmanik@gmail.com>
2023-03-14 23:29:15 +0800

    timeloop solution

---
+++ timeloop.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class timeloop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i + " Abracadabra");
+        }
+    }
+}

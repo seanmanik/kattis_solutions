477468e8f6437c2908126a7e8543fcf0d87b78ba
Sean <seanmanik@gmail.com>
2023-03-15 20:11:03 +0800

    numberfun solution

--- numberfun.java
+++ numberfun.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class numberfun {
+import java.util.*;
+
+public class numberfun {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        while (n > 0) {
+            int[] arr = new int[3];
+            arr[0] = scanner.nextInt();
+            arr[1] = scanner.nextInt();
+            arr[2] = scanner.nextInt();
+            Arrays.sort(arr);
+            if (arr[0] + arr[1] == arr[2]) {
+                System.out.println("Possible");
+            } else if (arr[0] * arr[1] == arr[2]) {
+                System.out.println("Possible");
+            } else {
+                System.out.println("Impossible");
+            }
+            n--;
+        }
+    }
 }

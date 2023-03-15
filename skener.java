3cbd7d1b05957df6fc75965b9e8543ab5942fdf0
Sean <seanmanik@gmail.com>
2023-03-15 20:40:50 +0800

    skener solution

--- skener.java
+++ skener.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class skener {
+import java.util.*;
+
+public class skener {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int r = scanner.nextInt();
+        int c = scanner.nextInt();
+        int zr = scanner.nextInt();
+        int zc = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i = 0; i < r; i++) {
+            String toPrint = "";
+            String s = scanner.nextLine();
+            for (int j = 0; j < c; j++) {
+                for (int k = 0; k < zc; k++) {
+                    toPrint += s.charAt(j);
+                }
+            }
+            for (int k = 0; k < zr; k++) {
+                System.out.println(toPrint);
+            }
+        }
+    }
 }

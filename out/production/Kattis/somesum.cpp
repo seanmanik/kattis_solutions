5005857e94c032af811396590767cf2fab883b29
Sean <seanmanik@gmail.com>
2023-03-20 10:40:56 +0800

    somesum solution

---
+++ out/production/Kattis/somesum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int k;
+    cin >> k;
+    if (k == 1)
+    {
+        cout << "Either";
+    }
+    else if (k % 2 == 0)
+    {
+        cout << "Odd";
+    }
+    else
+    {
+        cout << "Either";
+    }
+}

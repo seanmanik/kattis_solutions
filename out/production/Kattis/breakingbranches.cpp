229aab414527cf9f8bfa81f51c8fdd433a47d082
Sean <seanmanik@gmail.com>
2023-03-28 22:02:19 +0800

    breakingbranches solution

---
+++ out/production/Kattis/breakingbranches.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int k;
+    cin >> k;
+    if (k % 2 == 0)
+    {
+        cout << "Alice\n1";
+    }
+    else
+    {
+        cout << "Bob";
+    }
+}

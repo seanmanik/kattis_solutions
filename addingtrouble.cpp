3d64cd0fe923384aa4f90b3b6aeecc4eee968f37
Sean <seanmanik@gmail.com>
2023-04-09 23:41:43 +0800

    addingtrouble solution

---
+++ addingtrouble.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a + b == c)
+    {
+        cout << "correct!";
+    }
+    else
+    {
+        cout << "wrong!";
+    }
+}
